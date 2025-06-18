2a3c38fe6c0e14e81c434acb185c74bca2609873
Anas Abdelhakim Abdelgayd <117493389+anasabdelhakim@users.noreply.github.com>
2025-06-18 12:20:07 +1100

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-import styles from "./Loader.module.css";
+
 const Loader = () => {
-  return <h1 className={styles.loading}>loading...</h1>;
+  const loadingStyles = {
+    width: "100%",
+    height: "100%",
+    color: "white",
+    fontSize: "50px", // In JSX, CSS properties like 'font-size' become 'fontSize' (camelCase)
+    backgroundColor: "var(--movie-list-light)", // CSS variables are supported in inline styles
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return <h1 style={loadingStyles}>loading...</h1>;
 };
 
 export default Loader;
